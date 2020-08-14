e5865c3479828d61b3dde0e91ea70a1b1afd1d94
Run Chen <yma8882003@gmail.com>
2020-08-14 12:10:17 +0800

    RunChen

--- app.js
+++ app.js
@@ -1,36 +1,41 @@
 let express = require('express');
 let firebase = require('firebase')
 var firebaseConfig = {
-    apiKey: "AIzaSyDxCLJkEfGrDxup6-ROtrYbC_m7ocgqllE",
-    authDomain: "nodejs-class-practice.firebaseapp.com",
-    databaseURL: "https://nodejs-class-practice.firebaseio.com",
-    projectId: "nodejs-class-practice",
-    storageBucket: "nodejs-class-practice.appspot.com",
-    messagingSenderId: "1015240617673",
-    appId: "1:1015240617673:web:a1c2928cbe704d4baa2718",
-    measurementId: "G-KHL0Y789PP"
+    apiKey: "AIzaSyCjdBEJFC4UdjIhoXx4qslrjx7G1yqyJv8",
+    authDomain: "run-chen.firebaseapp.com",
+    databaseURL: "https://run-chen.firebaseio.com",
+    projectId: "run-chen",
+    storageBucket: "run-chen.appspot.com",
+    messagingSenderId: "366645597462",
+    appId: "1:366645597462:web:56d7c6ff86709f36202c37",
+    measurementId: "G-5B8DZEL2PR"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 let db = firebase.firestore();
 let app = express();
 app.use(express.static('./public'));
-app.set('view engine', 'ejs');  
-app.get('/', async (req, res) => {  
-    //let data = await db.collection('classA').get();
-    //let userArr = []
-    // data.forEach((doc) => {
-    //     console.log(doc.data().name)
-    //     userArr.push(doc.data().name);
-    // })
-    res.render('default', {  
-        title: '首頁',  
-        users: ['Fisheep', 'Fiona', 'Alice', 'Bob']
-        //users: userArr
-    });  
+app.set('view engine', 'ejs');
+app.get('/', async(req, res) => {
+    let data = await db.collection('classA').get();
+    let userArr = []
+    data.forEach((doc) => {
+        console.log(doc.data().name)
+        userArr.push(doc.data().name);
+    })
+    res.render('default', {
+        title: '首頁',
+        // users: ['Fisheep', 'Fiona', 'Alice', 'Bob']
+        users: userArr
+    });
 });
 
-app.get("/firebase-test", async (req, res) => {
+app.get("/runchen", (req, res) => {
+
+    res.send("<h1>RunChen</h1>")
+})
+
+app.get("/firebase-test", async(req, res) => {
     let html = '';
     let data = await db.collection('classA').get();
     data.forEach(doc => {
@@ -40,7 +45,7 @@ app.get("/firebase-test", async (req, res) => {
     res.send(html)
 })
 
-app.get("/classA_backend", async (req, res) => {
+app.get("/classA_backend", async(req, res) => {
     let data = await db.collection('classA').get();
     userArr = []
     data.forEach((doc) => {
@@ -58,16 +63,16 @@ app.get("/classA_backend", async (req, res) => {
 
 app.get("/classA_frontend", (req, res) => {
     let options = {
-        root:  __dirname+"/public",
+        root: __dirname + "/public",
         dotfiles: 'ignore'
     }
-    console.log(__dirname+"/public");
+    console.log(__dirname + "/public");
     res.sendFile("/classA.html", options);
 })
 
-app.get('/who/:name', (req, res) => {  
-    var name = req.params.name;  
-    res.send(`This is ${name}`);  
+app.get('/who/:name', (req, res) => {
+    var name = req.params.name;
+    res.send(`This is ${name}`);
 });
 
 app.get('/API/deleteMember', (req, res) => {
@@ -86,10 +91,10 @@ app.get('/API/addMember', (req, res) => {
     res.send("Add member success!");
 })
 
-app.get('*', (req, res) => {  
-    res.send('No Content');  
+app.get('*', (req, res) => {
+    res.send('No Content');
 });
 
-app.listen(3000, () => {  
-    console.log('Listening on port 3000');  
-}); 
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
--- render_test.js
+++ render_test.js
@@ -1,9 +1,34 @@
 let express = require('express')
 let app = express();
+
+let firebase = require('firebase')
+var firebaseConfig = {
+    apiKey: "AIzaSyCjdBEJFC4UdjIhoXx4qslrjx7G1yqyJv8",
+    authDomain: "run-chen.firebaseapp.com",
+    databaseURL: "https://run-chen.firebaseio.com",
+    projectId: "run-chen",
+    storageBucket: "run-chen.appspot.com",
+    messagingSenderId: "366645597462",
+    appId: "1:366645597462:web:56d7c6ff86709f36202c37",
+    measurementId: "G-5B8DZEL2PR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
+
+// 要渲染的檔案是哪個
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
+app.get('/', async(req, res) => {
+
+    let data = await db.collection("classA").get();
+    let userArr = [];
+    data.docs.forEach((doc) => {
+        userArr.push(doc.data().name);
+    });
+
     res.render("default", {
-        users: ["Alice", "Bob", "Fisheep", "Fiona"],
+        // users: ["Alice", "Bob", "Fisheep", "Fiona"],
+        users: userArr,
         title: "This is root page!"
     })
 })
